7940b3add7825a89ca0c2cdc6f6aaa1535cbbc26
Mike Hsu <mike@myquidd.com>
2017-07-24 01:44:55 -0400

    Started with adding custom view

---
+++ app/src/main/java/UI/CustomDrawView.java
@@ -0,0 +1,16 @@
+package UI;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by mikeh on 7/24/2017.
+ */
+
+public class CustomDrawView extends View {
+    public CustomDrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
